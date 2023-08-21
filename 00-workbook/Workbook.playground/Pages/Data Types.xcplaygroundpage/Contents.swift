0e4ea629cd3bd99b5b9cc70558def7b7c64d912d
Brent Crowley <brent.crowley@seventhbeam.com>
2023-08-21 11:07:43 +1000

    complete the data types lesson

---
+++ 00-workbook/Workbook.playground/Pages/Data Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,65 @@
+import Foundation
+//: ## Data Types
+//: ### Integers
+// UInt8 means that the integer can store 8 bits (1 byte).
+//
+
+let age: UInt8 = 32 // positive only
+MemoryLayout.size(ofValue: age)
+MemoryLayout<UInt8>.size
+UInt8.min
+UInt8.max
+let height: Int8 = 72
+MemoryLayout.size(ofValue: height)
+MemoryLayout<Int8>.size
+Int8.min
+Int8.max
+MemoryLayout<Int16>.size
+Int16.min
+Int16.max
+MemoryLayout<Int32>.size
+Int32.min
+Int32.max
+MemoryLayout<Int64>.size
+Int64.min
+Int64.max
+MemoryLayout<Int>.size
+Int.min
+Int.max
+//: ### Floats
+let weight: Float = 154.5
+
+// The larger a float number becomes, the lower the precision of the float.
+// Eg. A float range between 1-2 has high precision as memory can allocate space for many decimal places.
+// But a range between 1-10000 will have much less precision because of the number of decimal places it can hold.
+MemoryLayout.size(ofValue: weight)
+MemoryLayout<Float>.size
+Float.leastNormalMagnitude
+Float.greatestFiniteMagnitude
+//: ### Doubles
+// Uses twice the bits as a single precision number.
+let earthRadius: Double = 3958.8
+MemoryLayout.size(ofValue: earthRadius)
+MemoryLayout<Double>.size
+Double.leastNormalMagnitude
+Double.greatestFiniteMagnitude
+//: ### Binary & Hexadecimal
+// By deafult, when you use an integer literal, it's in base 10.
+// Using a 0b refix means that you're using a base 2 literal.
+// Split the number with an underscore in every 4 bits.
+let ageBinary: UInt8 = 0b0010_0000
+let ageBinaryNegative: Int8 = -0b0010_0000
+// A group of 4 bits is known as a nibble.
+// Often represented using a base 16 number. (Hexadecimal) 0-15 inclusive
+// prefix for hex is 0x
+let weightHexadecimal: UInt16 = 0x9B
+let weightHexadecimalNegative: Int16 = -0x9B
+//: ## Bytes
+let favoriteBytes: [UInt8] = [
+  240,          159,          152,          184,
+  240,          159,          152,          185,
+  0b1111_0000,  0b1001_1111,  0b1001_1000,  186,
+  0xF0,         0x9F,         0x98,         187
+]
+
+MemoryLayout<UInt8>.size * favoriteBytes.count
